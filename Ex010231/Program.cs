4ea89e9789195ee4a8279b42302c1510a3e53f1b
ElnazShaigany <53558368+Elishaii@users.noreply.github.com>
2022-05-23 23:10:21 +0430

    Add files via upload

---
+++ Ex010231/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace session1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            
+        for (int i=1 ; i<6 ;i++)
+        {
+         for   (int s=1; s<=6-i; s++)
+         {
+             Console.Write($" ");         
+         }  
+         for(int j=1; j<=i ; j++)
+         {
+             Console.Write( j );             
+         }
+         for (int k=(i-1); k>=1 ;k--)
+         {Console.Write( k);
+         }
+        Console.WriteLine();
+    
+    
+        }
+    }
+}
+}
---
+++ Ex010302/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ex010302
+{
+    class Program
+    {
+        static void Main()
+        {
+         int s = 0;  // for sum      
+         System.Console.Write($"Enter a number: ");
+        string num=Console.ReadLine();
+        long Number=Convert.ToInt64(num);
+        for (int i=0; i<= Number; i++){
+            if(i%2==1){
+                s=s+i;
+            }
+        }
+            Console.WriteLine($"Sum of odd number is :  {s}");
+
+        }
+    }
+}
+
+
+
