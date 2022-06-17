731c24245ac5ca380321786c365bd0bcdb639c98
ElnazShaigany <53558368+Elishaii@users.noreply.github.com>
2022-06-17 23:41:44 +0430

    Add files via upload

---
+++ Tamrin0326Asan/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace tamrin0326 {
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine($"Enter a base value:");
+            int a =Convert.ToInt32( Console.ReadLine());
+        Console.WriteLine($"Enter a value of power:");
+            int b =Convert.ToInt32( Console.ReadLine());
+            int result= power(a,b);
+            Console.WriteLine($"Result: {result}");
+            
+            
+        }
+    public static int power(int a, int b) {
+ 
+        if (b > 1) {
+            return (a*power(a, b-1));
+        }
+        else{
+            return a;
+        }
+         
+    }
+    }
+}
