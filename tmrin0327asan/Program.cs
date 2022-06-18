b687bb1a2b254c6d958da34ff512a4d20bf87cc3
ElnazShaigany <53558368+Elishaii@users.noreply.github.com>
2022-06-18 18:55:39 +0430

    Add files via upload

---
+++ tmrin0327asan/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace tmrin0327asan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        List<int> num = Enumerable.Range(1, 99).ToList();
+        num.ForEach(e=>Console.Write(e + ","));
+        Console.Write($"100.");
+        Console.WriteLine();
+        int sum = num.Where(n => n % 2 == 1).Sum();
+        System.Console.WriteLine($"sum of odd number is:{sum}"); 
+
+    }
+}
+}
